c28bd1577deb40a91b3b11c9b65c2ff6d1a3ed95
Steven Lei <stevenplus@gmail.com>
2022-12-16 00:01:04 +0800

    init routes

---
+++ pages/dashboard/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Quizard</title>
+        <meta name="description" content="Quiz Platform in the Web3" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="max-w-7xl mx-auto">
+        <h1 className="text-center text-2xl font-bold">Dashboard page</h1>
+      </main>
+    </div>
+  )
+}
